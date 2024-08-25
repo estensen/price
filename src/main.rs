b842a06fb5a4ec69d82c2214028e9dca3b383c9a
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-08-25 13:33:27 +0200

    Get price of ETH

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CoinGeckoResponse {
+    ethereum: Price,
+}
+
+#[derive(Deserialize)]
+struct Price {
+    usd: f64,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
+
+    let response: CoinGeckoResponse = reqwest::get(url).await?.json().await?;
+
+    println!("Current price of ETH is ${}", response.ethereum.usd);
+
+    Ok(())
 }
